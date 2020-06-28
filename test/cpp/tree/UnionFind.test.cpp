74afbb56b66f2590d7af9152d69c1f12cf4248e0
MitI_7 <MitI_7@outlook.com>
2020-06-29 00:35:26 +0900

    add union find

---
+++ test/cpp/tree/UnionFind.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
+#include "library/cpp/tree/UnionFind.hpp"
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, q;
+    cin >> n >> q;
+
+    UnionFind uf(n);
+    for (int i = 0; i < q; ++i) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if (com == 0) {
+            uf.union_set(x, y);
+        }
+        else {
+            cout << uf.is_same_set(x, y) << endl;
+        }
+
+    }
+
+    return 0;
+}
