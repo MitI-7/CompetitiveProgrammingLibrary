74373caafed28fbd5c49aad498509aa9277f17f6
MitI_7 <miti_7@outlook.com>
2023-05-15 18:41:34 +0900

    add find_cycle2.test.cpp

---
+++ test/cpp/Graph/find_cycle2.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cycle_detection"
+
+#include <iostream>
+#include "library/cpp/Graph/find_cycle.cpp"
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+    Graph<int> graph(N);
+    for (int i = 0; i < M; ++i) {
+        int U, V;
+        cin >> U >> V;
+        graph.add_directed_edge(U, V, 0, i);
+    }
+
+    auto cycle = find_cycle(graph);
+
+    if (cycle.empty()) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    cout << cycle.size() << endl;
+    for (auto e: cycle) {
+        cout << e.no << endl;
+    }
+
+    return 0;
+}
