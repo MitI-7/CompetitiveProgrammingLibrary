3723e1a449c56559c70854d0b09bcef807564419
MitI_7 <miti_7@outlook.com>
2023-05-18 13:20:18 +0900

    add kruskal.cpp

---
+++ library/cpp/Graph/kruskal.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <vector>
+#include "library/cpp/Tree/UnionFind.hpp"
+
+
+struct Edge {
+    int from;
+    int to;
+    long long cost;
+    int no;
+
+    Edge() {}
+
+    Edge(int from, int to, long long cost, int no = 0) : from(from), to(to), cost(cost), no(no) {}
+
+    bool operator<(const Edge &edge) const {
+        return this->cost < edge.cost;
+    }
+
+    bool operator>(const Edge &edge) const {
+        return this->cost > edge.cost;
+    }
+};
+
+// グラフの最小全域木を求める。O(|E| log|V|)
+long long kruskal(int num_of_node, std::vector<Edge> &edges) {
+    // コストの小さい順にソート
+    sort(edges.begin(), edges.end());
+
+    UnionFind uf(num_of_node);
+    long long ans = 0;
+    for (const auto e: edges) {
+        if (!uf.is_same_set(e.from, e.to)) {
+            uf.union_set(e.from, e.to);
+            ans += e.cost;
+        }
+    }
+
+    return ans;
+}
