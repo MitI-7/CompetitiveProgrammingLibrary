84262ad97767b730d67a84d88c40ca4d745eb38a
MitI_7 <MitI_7@outlook.com>
2024-01-27 12:23:47 +0900

    add SegmentTree2_point_update_range_min.test.cpp

---
+++ test/cpp/Tree/SegmentTree2_point_update_range_min.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
+
+#include "library/cpp/Tree/SegmentTree.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N, Q;
+    cin >> N >> Q;
+
+    auto st = make_segment_tree_min<long long>(N);
+    for (int i = 0; i < N; ++i) {
+        st.update(i, (1ull << 31u) - 1);
+    }
+
+    for (int i = 0; i < Q; ++i) {
+        int COM, X, Y;
+        cin >> COM >> X >> Y;
+        if (COM == 0) {
+            st.update(X, Y);
+        } else {
+            cout << st.query(X, Y + 1) << endl;
+        }
+    }
+
+    return 0;
+}
