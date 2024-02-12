f0b2b2a21f23093341a908d6649c2d947732c17f
MitI_7 <MitI_7@outlook.com>
2024-02-12 19:57:34 +0900

    add BinaryTrie2_erase.test.cpp

---
+++ test/cpp/Tree/BinaryTrie2_erase.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP2_7_B"
+
+#include "library/cpp/Tree/BinaryTrie.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int Q;
+    cin >> Q;
+
+    BinaryTrie<unsigned int, 30> bt;
+    for (int i = 0; i < Q; ++i) {
+        int QUERY, X;
+        cin >> QUERY >> X;
+        if (QUERY == 0) {
+            if (not bt.exist(X)) {
+                bt.insert(X);
+            }
+            cout << bt.size() << endl;
+        } else if (QUERY == 1) {
+            cout << bt.exist(X) << endl;
+        } else if (QUERY == 2) {
+            bt.erase(X);
+        }
+    }
+
+    return 0;
+}
