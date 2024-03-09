ef271f9e2631d8a6bba6cb06b3d1d6f0b8f6116b
MitI_7 <MitI_7@outlook.com>
2024-03-09 23:47:06 +0900

    add LazySegmentTree4_RSQ_RUQ.test.cpp

---
+++ test/cpp/Tree/LazySegmentTree4_RSQ_RUQ.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_I"
+
+#include "library/cpp/Tree/LazySegmentTree_template.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N, Q;
+    cin >> N >> Q;
+    
+    auto lst = make_range_sum_query_range_update_operation<long long>(N, 0);
+    for (int i = 0; i < Q; ++i) {
+        int TYPE;
+        cin >> TYPE;
+        if (TYPE == 0) {
+            int S, T, X;
+            cin >> S >> T >> X;
+            lst.operation(S, T + 1, X);
+        } else {
+            int S, T;
+            cin >> S >> T;
+            cout << lst.query(S, T + 1) << endl;
+        }
+    }
+
+    return 0;
+}
